902ea061fd9b9f393803076feea97ebff7ef1035
Doménique Tilleuil <domenique@tripled.io>
2022-02-18 11:18:51 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -35,7 +35,6 @@ public class Game {
 	}
 
 	public boolean add(String playerName) {
-	    players.add(playerName);
 		playerList.add(new Player(playerName));
 	    places[howManyPlayers()] = 0;
 	    purses[howManyPlayers()] = 0;
