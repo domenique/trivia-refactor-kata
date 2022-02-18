87da6cf8716a3046b55cad393d7fd09022d5275e
Michel Grootjans <michel.grootjans@gmail.com>
2022-02-18 11:04:43 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -99,9 +99,7 @@ public class Game {
 	
 	private String currentCategory() {
 		if (places[currentPlayer] % 4 == 0) return "Pop";
-		if (places[currentPlayer] == 1) return "Science";
-		if (places[currentPlayer] == 5) return "Science";
-		if (places[currentPlayer] == 9) return "Science";
+		if (places[currentPlayer] % 4 == 1) return "Science";
 		if (places[currentPlayer] == 2) return "Sports";
 		if (places[currentPlayer] == 6) return "Sports";
 		if (places[currentPlayer] == 10) return "Sports";
